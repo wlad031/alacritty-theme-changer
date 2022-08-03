47c8c13bd72b31f396e167cbc4f2b20f7edbdc50
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2022-08-03 16:03:38 +0200

    First version

---
+++ alacritty-theme-changer.sh
@@ -0,0 +1,19 @@
+# Expects first argument to be true or false
+# - true means changing theme from light to dark
+# - false means changing theme from dark to light
+
+light_theme="gruvbox_material_medium_light"
+dark_theme="gruvbox_material_medium_dark"
+
+if [ $1 == "true" ]; then
+    new_theme=$dark_theme
+    old_theme=$light_theme
+else
+    new_theme=$light_theme
+    old_theme=$dark_theme
+fi
+
+echo "Changing Alacritty theme from $old_theme to $new_theme..."
+ALACRITTY_DIR="$HOME/.config/alacritty"
+sed -i '' "s/\*$old_theme/\*$new_theme/g" "$ALACRITTY_DIR/alacritty.yml"
+
