f5ae4701452333c7c6c81a186acee1181223305c
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2022-08-04 13:27:32 +0200

    Update script

--- alacritty-theme-changer.sh
+++ alacritty-theme-changer.sh
@@ -13,7 +13,18 @@ else
     old_theme=$dark_theme
 fi
 
-echo "Changing Alacritty theme from $old_theme to $new_theme..."
 ALACRITTY_DIR="$HOME/.config/alacritty"
-sed -i '' "s/\*$old_theme/\*$new_theme/g" "$ALACRITTY_DIR/colors.yml"
+ALACRITTY_COLORS="$ALACRITTY_DIR/colors.yml"
+if [ -f "$ALACRITTY_COLORS" ]; then
+    cat "$ALACRITTY_COLORS" | grep "*$old_theme" &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Cannot find *$old_theme in $ALACRITTY_COLORS"
+        exit 1
+    fi
+    echo "Changing Alacritty theme from $old_theme to $new_theme..."
+    sed -i '' "s/\*$old_theme/\*$new_theme/g" "$ALACRITTY_COLORS"
+else
+    echo "$ALACRITTY_COLORS doesn't exist!"
+    exit 1
+fi
 
